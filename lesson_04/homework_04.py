532ca2a2e451ae97759cb2539c3c4885ee2b9937
kassxxv <linktdev@gmai.com>
2026-01-01 13:04:51 +0100

    finish homework 04

--- lesson_04/homework_04.py
+++ lesson_04/homework_04.py
@@ -23,48 +23,60 @@ rolling in wealth."""
 # task 01 ==
 """ Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
 треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace('\n', ' ')
 
 # task 02 ==
 """ Замініть .... на пробіл
 """
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace('....', ' ')
 
 # task 03 ==
 """ Зробіть так, щоб у тексті було не більше одного пробілу між словами.
 """
-
+adwentures_of_tom_sawer = ' '.join(adwentures_of_tom_sawer.split())
 
 # task 04
 """ Виведіть, скількі разів у тексті зустрічається літера "h"
 """
-
+print(adwentures_of_tom_sawer.count('h'))
 
 # task 05
 """ Виведіть, скільки слів у тексті починається з Великої літери?
 """
-
+count = 0
+words = adwentures_of_tom_sawer.split()
+for word in words:
+    if (word.strip('"'))[0].isupper():
+        count += 1
+print(count)
 
 # task 06
 """ Виведіть позицію, на якій слово Tom зустрічається вдруге
 """
-
+first_position = adwentures_of_tom_sawer.find('Tom')
+second_position = adwentures_of_tom_sawer.find('Tom', first_position + 1)
+print(second_position)
 
 # task 07
 """ Розділіть змінну adwentures_of_tom_sawer по кінцю речення.
 Збережіть результат у змінній adwentures_of_tom_sawer_sentences
 """
-adwentures_of_tom_sawer_sentences = None
+adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split('. ')
+print(adwentures_of_tom_sawer_sentences)
 
 # task 08
 """ Виведіть четверте речення з adwentures_of_tom_sawer_sentences.
 Перетворіть рядок у нижній регістр.
 """
-
+print(adwentures_of_tom_sawer_sentences[3].lower())
 
 # task 09
 """ Перевірте чи починається якесь речення з "By the time".
 """
-
+for idx, sentence in enumerate(adwentures_of_tom_sawer_sentences):
+    print(f" Речення #{idx+1} починається з \"By the time\" {sentence.startswith('By the time')}")
 
 # task 10
 """ Виведіть кількість слів останнього речення з adwentures_of_tom_sawer_sentences.
 """
+print(f'Кількість слів в в останньому реченні: {len(adwentures_of_tom_sawer_sentences[-1].split())}')
